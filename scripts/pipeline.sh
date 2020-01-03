79968147dbd9442eb685117a3f9e4d371bbd1ced
simonepri <simonepri@outlook.com>
2020-01-03 01:30:36 +0100

    Add download_fever task

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -30,6 +30,47 @@ function install_deps() {
 }
 
 
+# Download the FEVER dataset and the pre-processed Wikipedia articles from the
+# website of the FEVER share task.
+function download_fever() {
+  local fever_path=$1
+  local pipeline_path=$2
+  local cache_path=$3
+  local force=$4
+  local download=$5
+
+  local dataset_path="$fever_path/dataset"
+  local wikipedia_path="$fever_path/wikipedia"
+
+  if (( $force != 0 )); then
+    rm -rf "$dataset_path"
+    rm -rf "$wikipedia_path"
+  fi
+
+  if [ ! -d "$dataset_path" ]; then
+    mkdir -p "$dataset_path"
+
+    echo '● Downloading the FEVER dataset...'
+    wget -q --show-progress --progress=bar:force -O "$dataset_path/train.jsonl" \
+    'https://s3-eu-west-1.amazonaws.com/fever.public/train.jsonl'
+    wget -q --show-progress --progress=bar:force -O "$dataset_path/dev.jsonl" \
+    'https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_dev.jsonl'
+    wget -q --show-progress --progress=bar:force -O "$dataset_path/test.jsonl" \
+    'https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_test.jsonl'
+  fi
+
+  if [ ! -d "$wikipedia_path" ]; then
+    mkdir -p "$wikipedia_path"
+
+    echo '● Downloading the FEVER pre-processed Wikipedia articles...'
+    wget -q --show-progress --progress=bar:force -O "$fever_path/wikipedia.zip" \
+    'https://s3-eu-west-1.amazonaws.com/fever.public/wiki-pages.zip'
+    unzip -o -j "$fever_path/wikipedia.zip" 'wiki-pages/*' -d "$wikipedia_path"
+    rm "$fever_path/wikipedia.zip"
+  fi
+}
+
+
 # Run the pipeline
 function run() {
   # Read all the recognized flags and expected arguments.
@@ -63,6 +104,9 @@ function run() {
   # Execute the tasks
   if [ -z $parg_task ] || [[ $parg_task == "install_deps" ]]; then
     install_deps "$PATH_D_FEVER" "$PATH_D_PIPELINE" "$PATH_D_CACHE" $flag_force $flag_download > >(tee -a "$PATH_D_LOGS/install_deps.log") 2>&1
+  fi
+  if [ -z $parg_task ] || [[ $parg_task == "download_fever" ]]; then
+    download_fever "$PATH_D_FEVER" "$PATH_D_PIPELINE" "$PATH_D_CACHE" $flag_force $flag_download > >(tee -a "$PATH_D_LOGS/download_fever.log") 2>&1
   fi
 }
 
